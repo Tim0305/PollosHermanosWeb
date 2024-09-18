320a5f0579a2997caed23b5cf945fd8fc62887ac
Timoteo <tabtim1015@gmail.com>
2024-09-18 13:57:31 -0600

    WIP: Creacion de componentes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,24 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'catalogo',
+    loadComponent: () =>
+      import('./components/catalog-list/catalog-list.component').then(
+        (m) => m.CatalogListComponent
+      ),
+  },
+  {
+    path: 'carrito',
+    loadComponent: () =>
+      import('./components/shopping-cart/shopping-cart.component').then(
+        (m) => m.ShoppingCartComponent
+      ),
+  },
+
+  {
+    path: '',
+    redirectTo: 'catalogo',
+    pathMatch: 'full',
+  },
+];
